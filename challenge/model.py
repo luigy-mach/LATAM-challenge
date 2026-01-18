21e93dbbdc29ec0cf031db7f3998cd9aced25559
Luigy Machaca <luigymachaca@gmail.com>
2026-01-18 05:16:23 -0500

    feat(mode.py): add _get_period_day method to FeatureGeneration

--- challenge/model.py
+++ challenge/model.py
@@ -1,11 +1,34 @@
 import pandas as pd
 
+from datetime import datetime
 from typing import Tuple, Union, List
 
 class FeatureGeneration:
     
     def __init__(self, data: pd.DataFrame):
         self.data = data.copy()
+        
+    def _get_period_day(self, date:str) -> str: 
+        date_time     = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
+        morning_min   = datetime.strptime("05:00", '%H:%M').time()
+        morning_max   = datetime.strptime("11:59", '%H:%M').time()
+        afternoon_min = datetime.strptime("12:00", '%H:%M').time()
+        afternoon_max = datetime.strptime("18:59", '%H:%M').time()
+        evening_min   = datetime.strptime("19:00", '%H:%M').time()
+        evening_max   = datetime.strptime("23:59", '%H:%M').time()
+        night_min     = datetime.strptime("00:00", '%H:%M').time()
+        night_max     = datetime.strptime("4:59", '%H:%M').time()
+        
+        if(date_time >= morning_min and date_time <= morning_max):
+            return 'mañana'
+        elif(date_time >= afternoon_min and date_time <= afternoon_max):
+            return 'tarde'
+        elif(
+            (date_time >= evening_min and date_time <= evening_max) or
+            (date_time >= night_min and date_time <= night_max)
+        ):
+            return 'noche'
+
 
 class DelayModel:
 
